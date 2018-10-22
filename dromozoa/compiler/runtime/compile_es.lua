bdc5ac72bca24a619a87d45a58aa786d2ec47bbe
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-10-23 08:03:12 +0900

    ランタイム生成を追加。

---
+++ dromozoa/compiler/runtime/compile_es.lua
@@ -0,0 +1,54 @@
+return [[
+let runtime_env = function () {
+  let tostring = function (v) {
+    const t = typeof v;
+    if (t === "undefined") {
+      return "nil";
+    } else if (t === "number") {
+      return v.toString();
+    } else if (t === "string") {
+      return v;
+    } else if (t === "boolean") {
+      if (v) {
+        return "true";
+      } else {
+        return "false";
+      }
+    } else if (t === "object") {
+      if (Map.prototype.isPrototypeOf(v)) {
+        return "table";
+      }
+    } else if (t === "function") {
+      return "function";
+    }
+    return "userdata";
+  };
+
+  let env = new Map();
+
+  env.set("print", function (...vararg) {
+    for (let i = 0; i < vararg.length; ++i) {
+      if (i > 0) {
+        process.stdout.write("\t");
+      }
+      process.stdout.write(tostring(vararg[i]));
+    }
+    process.stdout.write("\n");
+    return [];
+  });
+
+  env.set("assert", function (...vararg) {
+    let v = vararg[0];
+    if (v === undefined || v === false) {
+      if (vararg.length > 1) {
+        throw vararg[1];
+      } else {
+        throw "assertion failed"
+      }
+    }
+    return vararg;
+  });
+
+  return env;
+};
+]]
