0ae13c68b3f883d767722e292e636d44a7fb9dd0
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-10-24 12:08:15 +0900

    ESランタイムを修正。

--- dromozoa/compiler/runtime/runtime_es.lua
+++ dromozoa/compiler/runtime/runtime_es.lua
@@ -91,31 +91,33 @@ const tostring = function (v) {
   return "userdata";
 };
 
-const type = function (v) {
-  const t = typeof v;
-  if (t === "undefined") {
-    return "nil";
-  } else if (t === "number") {
-    return "number"
-  } else if (t === "string") {
-    return "string"
-  } else if (t === "boolean") {
-    return "boolean"
-  } else if (t === "object") {
-    if (Map.prototype.isPrototypeOf(v)) {
-      return "table";
-    }
-  } else if (t === "function") {
-    return "function";
-  }
-  return "userdata";
-}
-
 const GETTABLE = function (table, index) {
-  return table.get(index);
+  const result = table.get(index);
+  if (result === undefined) {
+    const field = getmetafield(table, "__index");
+    if (field !== undefined) {
+      if (typeof field === "function") {
+        return CALL1(field, table, index);
+      } else {
+        return GETTABLE(field, index);
+      }
+    }
+  }
+  return result;
 };
 
 const SETTABLE = function (table, index, value) {
+  const result = table.get(index);
+  if (result === undefined) {
+    const field = getmetafield(table, "__newindex");
+    if (field !== undefined) {
+      if (typeof field === "function") {
+        return CALL0(field, table, index, value);
+      } else {
+        return SETTABLE(field, index, value);
+      }
+    }
+  }
   if (value === undefined) {
     table.delete(index);
   } else {
@@ -127,6 +129,10 @@ const LEN = function (v) {
   const t = typeof v;
   if (t === "object") {
     if (Map.prototype.isPrototypeOf(v)) {
+      const field = getmetafield(v, "__len");
+      if (field !== undefined) {
+        return CALL1(field, v);
+      }
       for (let i = 1; ; ++i) {
         if (v.get(i) === undefined) {
           return i - 1;
@@ -146,9 +152,28 @@ const open_env = function () {
   const env = new Map();
 
   env.set("tostring", tostring);
-  env.set("type", type);
   env.set("getmetatable", getmetatable);
   env.set("setmetatable", setmetatable);
+
+  env.set("type", function (v) {
+    const t = typeof v;
+    if (t === "undefined") {
+      return "nil";
+    } else if (t === "number") {
+      return "number"
+    } else if (t === "string") {
+      return "string"
+    } else if (t === "boolean") {
+      return "boolean"
+    } else if (t === "object") {
+      if (Map.prototype.isPrototypeOf(v)) {
+        return "table";
+      }
+    } else if (t === "function") {
+      return "function";
+    }
+    return "userdata";
+  });
 
   env.set("print", function (...args) {
     for (let i = 0; i < args.length; ++i) {
@@ -164,9 +189,9 @@ const open_env = function () {
     const v = args[0];
     if (v === undefined || v === false) {
       if (args.length > 1) {
-        throw args[1];
+        throw new Error(args[1]);
       } else {
-        throw "assertion failed";
+        throw new Error("assertion failed");
       }
     }
     return args;
