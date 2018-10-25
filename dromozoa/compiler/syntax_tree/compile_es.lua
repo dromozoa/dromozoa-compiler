95efd4c9f4634519bddf8dd3f9db2b82ab53e20b
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-10-25 17:07:27 +0900

    リファクタリング。

--- dromozoa/compiler/syntax_tree/compile_es.lua
+++ dromozoa/compiler/syntax_tree/compile_es.lua
@@ -33,59 +33,135 @@ for byte = 0x00, 0xFF do
   end
 end
 
+local var_table = {
+  V = "...V";
+  T = "...T";
+  NIL = "undefined";
+  FALSE = "false";
+  TRUE = "true";
+}
+
 local function encode_string(s)
   return "\"" .. s:gsub("[%z\1-\31\127]", char_table):gsub("\226\128[\168\169]", char_table) .. "\""
 end
 
-local function _(var, not_spread)
-  if var == "V" then
-    if not_spread then
-      return var
-    else
-      return "...V"
-    end
-  elseif var == "T" then
-    if not_spread then
-      return var
-    else
-      return "...T"
-    end
-  elseif var == "NIL" then
-    return "undefined"
-  elseif var == "FALSE" then
-    return "false"
-  elseif var == "TRUE" then
-    return "true"
+local function encode_var(var)
+  local result = var_table[var]
+  if result then
+    return result
   else
-    local k = var:sub(1, 1)
-    if k == "P" then
+    local key = var:sub(1, 1)
+    if key == "P" then
       return var
-    elseif k == "U" then
-      local i = var:sub(2)
-      return "U[" .. i .. "][0][U[" .. i .. "][1]]"
+    elseif key == "U" then
+      local index = var:sub(2)
+      return "U[" .. index .. "][0][U[" .. index .. "][1]]"
     else
-      return k .. "[" .. var:sub(2) .. "]"
+      return key .. "[" .. var:sub(2) .. "]"
     end
   end
 end
 
+local function encode_var_not_spread(var)
+  if var == "V" or var == "T" then
+    return var
+  else
+    return encode_var(var)
+  end
+end
+
+local function encode_vars(source, i, j)
+  if not i then
+    i = 1
+  end
+  if not j then
+    j = #source
+  end
+  local result = {}
+  for i = i, j do
+    result[#result + 1] = encode_var(source[i])
+  end
+  return table.concat(result, ", ")
+end
+
+local function _1(template)
+  return function (out, code)
+    out:write(template:format(encode_var(code[1])), ";\n")
+  end
+end
+
+local function _12(template)
+  return function (out, code)
+    out:write(template:format(encode_var(code[1]), encode_var(code[2])), ";\n")
+  end
+end
+
+local function _122(template)
+  return function (out, code)
+    local a = encode_var(code[1])
+    local b = encode_var(code[2])
+    out:write(template:format(a, b, b), ";\n")
+  end
+end
+
+local function _123(template)
+  return function (out, code)
+    out:write(template:format(encode_var(code[1]), encode_var(code[2]), encode_var(code[3])), ";\n")
+  end
+end
+
+local templates = {
+  MOVE     = _12  "%s = %s";
+  GETTABLE = _123 "%s = GETTABLE(%s, %s)";
+  SETTABLE = _123 "SETTABLE(%s, %s, %s)";
+  NEWTABLE = _1   "%s = new Map()";
+  ADD      = _123 "%s = %s + %s";
+  SUB      = _123 "%s = %s - %s";
+  MUL      = _123 "%s = %s * %s";
+  MOD      = _123 "%s = %s %% %s";
+  POW      = _123 "%s = %s ** %s";
+  DIV      = _123 "%s = %s / %s";
+  IDIV     = _123 "%s = Math.floor(%s / %s)";
+  BAND     = _123 "%s = %s & %s";
+  BOR      = _123 "%s = %s | %s";
+  BXOR     = _123 "%s = %s ^ %s";
+  SHL      = _123 "%s = %s << %s";
+  SHR      = _123 "%s = %s >>> %s";
+  UNM      = _12  "%s = -%s";
+  BNOT     = _12  "%s = ~%s";
+  NOT      = _122 "%s = %s === undefined || %s === false";
+  LEN      = _12  "%s = LEN(%s)";
+  CONCAT   = _123 "%s = tostring(%s) + tostring(%s)";
+  EQ       = _123 "%s = %s === %s";
+  NE       = _123 "%s = %s !== %s";
+  LT       = _123 "%s = %s < %s";
+  LE       = _123 "%s = %s <= %s";
+  TONUMBER = _12  "%s = tonumber(%s)";
+}
+
 local compile_proto
+local compile_code
+
+local function write_block(self, out, code)
+  for i = 1, #code do
+    compile_code(self, out, code[i])
+  end
+end
 
 function compile_code(self, out, code)
   local name = code[0]
   if code.block then
     if name == "LOOP" then
       out:write "while (true) {\n"
-      for i = 1, #code do
-        compile_code(self, out, code[i])
-      end
+      write_block(self, out, code)
       out:write "}\n"
     elseif name == "COND" then
       local cond = code[1]
+      local a = encode_var(cond[1])
       if cond[2] == "TRUE" then
-        out:write(("if (%s !== undefined && %s !== false) {\n"):format(_(cond[1]), _(cond[1])))
+        out:write(("if (%s !== undefined && %s !== false) {\n"):format(a, a))
       else
-        out:write(("if (%s === undefined || %s === false) {\n"):format(_(cond[1]), _(cond[1])))
+        out:write(("if (%s === undefined || %s === false) {\n"):format(a, a))
       end
       compile_code(self, out, code[2])
       if #code == 2 then
@@ -96,102 +172,40 @@ function compile_code(self, out, code)
         out:write "}\n"
       end
     else
-      for i = 1, #code do
-        compile_code(self, out, code[i])
-      end
+      write_block(self, out, code)
     end
   else
-    if name == "MOVE" then
-      out:write(("%s = %s;\n"):format(_(code[1]), _(code[2])))
-    elseif name == "GETTABLE" then
-      out:write(("%s = GETTABLE(%s, %s);\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "SETTABLE" then
-      out:write(("SETTABLE(%s, %s, %s);\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "NEWTABLE" then
-      out:write(("%s = new Map();\n"):format(_(code[1])))
-    elseif name == "ADD" then
-      out:write(("%s = %s + %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "SUB" then
-      out:write(("%s = %s - %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "MUL" then
-      out:write(("%s = %s * %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "MOD" then
-      out:write(("%s = %s %% %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "POW" then
-      out:write(("%s = %s ** %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "DIV" then
-      out:write(("%s = %s / %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "IDIV" then
-      out:write(("%s = Math.floor(%s / %s);\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "BAND" then
-      out:write(("%s = %s & %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "BOR" then
-      out:write(("%s = %s | %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "BXOR" then
-      out:write(("%s = %s ^ %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "SHL" then
-      out:write(("%s = %s << %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "SHR" then
-      out:write(("%s = %s >>> %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "UNM" then
-      out:write(("%s = -%s;\n"):format(_(code[1]), _(code[2])))
-    elseif name == "BNOT" then
-      out:write(("%s = ~%s;\n"):format(_(code[1]), _(code[2])))
-    elseif name == "NOT" then
-      out:write(("%s = %s === undefined || %s === false;\n"):format(_(code[1]), _(code[2]), _(code[2])))
-    elseif name == "LEN" then
-      out:write(("%s = LEN(%s);\n"):format(_(code[1]), _(code[2])))
-    elseif name == "CONCAT" then
-      out:write(("%s = tostring(%s) + tostring(%s);\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "EQ" then
-      out:write(("%s = %s === %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "NE" then
-      out:write(("%s = %s !== %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "LT" then
-      out:write(("%s = %s < %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "LE" then
-      out:write(("%s = %s <= %s;\n"):format(_(code[1]), _(code[2]), _(code[3])))
-    elseif name == "CALL" then
-      local args = {}
-      for i = 2, #code do
-        args[i - 1] = _(code[i])
-      end
+    if name == "CALL" then
+      local args = encode_vars(code, 2)
       local var = code[1]
       if var == "NIL" then
-        out:write(("CALL0(%s);\n"):format(table.concat(args, ", ")))
+        out:write(("CALL0(%s);\n"):format(args))
       elseif var == "T" then
-        out:write(("T = CALL(%s);\n"):format(table.concat(args, ", ")))
+        out:write(("T = CALL(%s);\n"):format(args))
       else
-        out:write(("%s = CALL1(%s);\n"):format(_(var), table.concat(args, ", ")))
+        out:write(("%s = CALL1(%s);\n"):format(encode_var(var), args))
       end
     elseif name == "RETURN" then
       local n = #code
       if n == 0 then
         out:write "return;\n"
       elseif n == 1 then
-        out:write(("return %s;\n"):format(_(code[1], true)))
+        out:write(("return %s;\n"):format(encode_var_not_spread(code[1])))
       else
-        local rets = {}
-        for i = 1, #code do
-          rets[i] = _(code[i])
-        end
-        out:write(("return [%s];\n"):format(table.concat(rets, ", ")))
+        out:write(("return [%s];\n"):format(encode_vars(code)))
       end
     elseif name == "SETLIST" then
-      out:write(("SETLIST(%s, %d, %s);\n"):format(_(code[1]), code[2], _(code[3])))
+      out:write(("SETLIST(%s, %d, %s);\n"):format(encode_var(code[1]), code[2], encode_var(code[3])))
     elseif name == "CLOSURE" then
       compile_proto(self, out, code[1])
     elseif name == "LABEL" then
-      -- TODO warn
+      out:write(("%s:\n"):format(code[1]))
     elseif name == "BREAK" then
       out:write "break;\n"
     elseif name == "GOTO" then
-      -- TODO warn
-    elseif name == "TONUMBER" then
-      out:write(("%s = tonumber(%s);\n"):format(_(code[1]), _(code[2])))
+      out:write(("// GOTO %s\n"):format(code[1]))
     else
-      -- DEBUG
-      error("invalid name " .. name)
+      templates[name](out, code)
     end
   end
 end
@@ -218,7 +232,7 @@ function compile_proto(self, out, name)
   if proto.vararg then
     pars[#pars + 1] = "...V"
   end
-  out:write(("const %s = (%s) => {\n"):format(_(proto[1]), table.concat(pars, ", ")))
+  out:write(("const %s = (%s) => {\n"):format(proto[1], table.concat(pars, ", ")))
 
   local constants = proto.constants
   local n = #constants
