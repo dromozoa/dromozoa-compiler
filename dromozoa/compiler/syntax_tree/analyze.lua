3769b24bcfdd5ebc18dba35aecd4592f0e04d7ce
Tomoyuki Fujimori <moyu@dromozoa.com>
2019-05-02 15:32:59 +0900

    for文のコード生成を修正。

--- dromozoa/compiler/syntax_tree/analyze.lua
+++ dromozoa/compiler/syntax_tree/analyze.lua
@@ -605,6 +605,9 @@ local function resolve_vars(self, node, symbol_table)
         assign_var(node);
         assign_var(node);
         assign_var(node);
+        assign_var(node);
+        assign_var(node);
+        assign_var(node);
         ref_constant(node, "string", "'for' initial value must be a number")[1];
         ref_constant(node, "string", "'for' limit must be a number")[1];
         ref_constant(node, "string", "'for' step must be a number")[1];
