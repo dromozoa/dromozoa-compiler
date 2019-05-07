958d15521e790a0c40526a7b766495806834a80e
Tomoyuki Fujimori <moyu@dromozoa.com>
2019-05-07 10:59:38 +0900

    binopの扱いを変更。

--- dromozoa/compiler/syntax_tree/analyze.lua
+++ dromozoa/compiler/syntax_tree/analyze.lua
@@ -556,10 +556,19 @@ local function resolve_names(self, node, symbol_table)
   return node
 end
 
+local function opname_to_message(opname)
+  if opname == "arithmetic" or opname == "bitwise" then
+    return "attempt to perform " .. opname .. " operation on a "
+  else
+    return "attempt to perform " .. opname .. " on a "
+  end
+end
+
 local function resolve_vars(self, node, symbol_table)
   local symbol = node[0]
   local n = #node
   local binop = node.binop
+  local opname = node.opname
 
   for i = 1, n do
     resolve_vars(self, node[i], symbol_table)
@@ -672,6 +681,20 @@ local function resolve_vars(self, node, symbol_table)
     node.var = assign_var(node.parent)
   elseif binop == "AND" or binop == "OR" then
     node.var = assign_var(node, "B")
+  elseif binop and opname then
+    node.var = assign_var(node)
+    node.vars = space_separated {
+      assign_var(node, "B");
+      assign_var(node, "B");
+      assign_var(node, "B");
+      assign_var(node);
+      assign_var(node);
+      assign_var(node);
+      assign_var(node);
+      ref_constant(node, "string", "__" .. binop:lower())[1];
+      ref_constant(node, "string", opname_to_message(opname))[1];
+      ref_constant(node, "string", " value")[1];
+    }
   elseif symbol == symbol_table.fieldlist or binop or node.unop then
     node.var = assign_var(node)
   end
