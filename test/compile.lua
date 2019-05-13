fbffa4d7704d2dd133fc45fe1e973684c2799d16
Tomoyuki Fujimori <moyu@dromozoa.com>
2019-05-13 15:09:46 +0900

    テストを修正。

--- test/compile.lua
+++ test/compile.lua
@@ -20,107 +20,144 @@ local lua53_lexer = require "dromozoa.compiler.lua53_lexer"
 local lua53_parser = require "dromozoa.compiler.lua53_parser"
 local syntax_tree = require "dromozoa.compiler.syntax_tree"
 
+local q = [[']]
+local e = [[\']]
+local qeq = q .. e .. q
+local eqq = e .. q .. q
+local qqe = q .. q .. e
+
+local function shell_quote(v)
+  return ((q .. tostring(v):gsub(q, qeq) .. q):gsub(eqq, e):gsub(qqe, e))
+end
+
+io.stdout:setvbuf "no"
+io.stderr:setvbuf "no"
+
 local lexer = lua53_lexer()
 local parser = lua53_parser()
 
-local source_file, output_name = ...
-local handle = assert(io.open(source_file))
-local source = handle:read "*a"
-handle:close()
+local function compile(source_file, output_name)
+  local handle = assert(io.open(source_file))
+  local source = handle:read "*a"
+  handle:close()
 
-local terminal_nodes, message, i = lexer(source)
-if not terminal_nodes then
-  error(error_message(message, source, i, source_file))
-end
-local accepted_node, message, i = parser(terminal_nodes)
-if not accepted_node then
-  for i = 1, #terminal_nodes do
-    local node = terminal_nodes[i]
-    print(parser.symbol_names[node[0]], node.p, node.i, node.j)
+  local terminal_nodes, message, i = lexer(source)
+  if not terminal_nodes then
+    error(error_message(message, source, i, source_file))
   end
-  error(error_message(message, source, i, source_file))
-end
+  local accepted_node, message, i = parser(terminal_nodes)
+  if not accepted_node then
+    for i = 1, #terminal_nodes do
+      local node = terminal_nodes[i]
+      print(parser.symbol_names[node[0]], node.p, node.i, node.j)
+    end
+    error(error_message(message, source, i, source_file))
+  end
 
-local t = syntax_tree(parser, source, terminal_nodes, accepted_node)
-local result, message, i = t:analyze()
-if not result then
-  error(error_message(message, source, i, source_file))
-end
+  local t = syntax_tree(parser, source, terminal_nodes, accepted_node)
+  local result, message, i = t:analyze()
+  if not result then
+    error(error_message(message, source, i, source_file))
+  end
 
-if not os.getenv "NO_GENERATE" then
   t:generate()
   for i = 1, #t.protos do
     t.protos[i]:generate()
   end
-end
 
-if not os.getenv "NO_COMPILE2" then
-  local out = assert(io.open(output_name .. "2.cpp", "w"))
-  out:write [[
-#include <iostream>
-#include "runtime.hpp"
-namespace {
-using namespace dromozoa::runtime;
-]]
-
-  for i = #t.protos, 1, -1 do
-    local proto = t.protos[i]
-    proto:generate_cxx(out)
-  end
-
-  out:write [[
-}
-int main(int, char*[]) {
-  using namespace dromozoa::runtime;
-
-  state_t state;
-  state.open_base();
-  state.open_io();
-  ref_t env { state.env() };
-  value_t proto(std::make_shared<P0>(env));
-  try {
-    std::shared_ptr<thunk_t> thunk = proto.call([](state_t, array_t) -> std::shared_ptr<thunk_t>{
-      return nullptr;
-    }, state, {});
-    while (thunk) {
-      thunk = (*thunk)();
-    }
-    return 0;
-  } catch (const value_t& e) {
-    std::cerr << e.checkstring() << "\n";
-    return 1;
-  }
-}
-]]
-
-  out:close()
-end
-
-if not os.getenv "NO_DUMP" then
   t:dump_tree(output_name .. ".html")
   t:dump_protos(output_name .. "-protos.html")
   for i = 1, #t.protos do
     local proto = t.protos[i]
     proto:dump(output_name .. "-" .. proto[1]:encode() .. ".html", true)
   end
+
+  return t
 end
 
-local html = os.getenv "OUTPUT_HTML"
-local name = os.getenv "OUTPUT_NAME"
-if html then
-  local out = assert(io.open(html, "w"))
+local output_dir = ...
+local out = assert(io.open(output_dir .. "/index.html", "w"))
+out:setvbuf "no"
+
+out:write [[
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>dromozoa-compiler</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css">
+<style>
+.markdown-body {
+  box-sizing: border-box;
+  min-width: 200px;
+  max-width: 980px;
+  margin: 0 auto;
+  padding: 45px;
+}
+@media (max-width: 767px) {
+  .markdown-body {
+    padding: 15px;
+  }
+}
+</style>
+</head>
+<body>
+<div class="markdown-body">
+<h1>dromozoa-compiler</h1>
+<h2>execute</h2>
+<ul>
+]]
+
+for i = 2, #arg do
+  local source_file = arg[i]
+  local source_name = assert(source_file:match "([^/]+)%.lua$")
+  local output_name = output_dir .. "/" .. source_name
+
+  io.write(("compiling %s...\n"):format(source_file))
+  local t = compile(source_file, output_name, out)
+  local protos = t.protos
+
+  -- generate lua
+  -- generate cxx
+  -- generate es
+
+  io.write(("executing %s...\n"):format(source_file))
+  local result = os.execute(("lua %s >%s 2>&1"):format(
+      shell_quote(source_file),
+      shell_quote(output_name .. "-expected.txt")))
+
+  -- execute lua
+  -- execute cxx
+  -- execute es
+
   out:write(([[
-  <a href="%s.js">es</a>,
-  <a href="%s.cpp">cxx</a>,
-  <a href="%s.html">tree</a>,
-  <a href="%s-protos.html">protos</a>,
-  bb:
-]]):format(name, name, name, name))
-  for i = 1, #t.protos do
-    local proto = t.protos[i]
+  <li>
+    %s
+    <a href="%s-tree.html">tree</a>,
+    <a href="%s-protos.html">protos</a>,
+    bb:
+]]):format(source_name, source_name, source_name))
+  for i = 1, #protos do
+    local proto_name = protos[i][1]:encode()
     out:write(([[
-  <a href="%s-%s.html">%s</a>,
-]]):format(name, proto[1]:encode(), proto[1]:encode()))
+    <a href="%s-%s.html">%s</a>,
+]]):format(source_name, proto_name, proto_name))
   end
-  out:close()
+
+  -- output lua
+  -- output cxx
+  -- output es
+
+  out:write(([[
+    result:
+    <a href="%s-expected.txt">expected</a>
+  </li>
+]]):format(source_name))
 end
+
+out:write [[
+</ul>
+</body>
+</html>
+]]
