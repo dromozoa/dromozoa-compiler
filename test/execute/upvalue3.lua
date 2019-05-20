5fd42ce13bc92e852c763c5a39150120097975e2
Tomoyuki Fujimori <moyu@dromozoa.com>
2019-05-20 17:23:42 +0900

    テストを追加。

---
+++ test/execute/upvalue3.lua
@@ -0,0 +1,23 @@
+-- Copyright (C) 2019 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-compiler.
+--
+-- dromozoa-compiler is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-compiler is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-compiler.  If not, see <http://www.gnu.org/licenses/>.
+
+local function f(a, b, c)
+  local function g()
+    a = 1
+  end
+  b = 2
+end
